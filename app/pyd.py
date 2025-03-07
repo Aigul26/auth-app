c39a36b10d4363f3da98ef94e27e95860e158738
Айгуль Абдуллова <abdullova26@yandex.ru>
2025-03-07 10:25:43 +0300

    validation fix

--- app/pyd.py
+++ app/pyd.py
@@ -1,10 +1,15 @@
-from datetime import datetime
-from typing import Optional
-
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
+import re
 
 
 class UserBase(BaseModel):
     phone: str = Field(..., max_length=12, examples=['+79876543210'])
     firstname: str = Field(..., examples=['Ivan'])
     lastname: str = Field(..., examples=['Ivanov'])
+
+
+    @field_validator('phone')
+    def validate_phone_format(cls, value):
+        if not re.match(r'^\+7\d{10}$', value):
+            raise ValueError('Номер телефона должен быть в формате +79876543210')
+        return value
